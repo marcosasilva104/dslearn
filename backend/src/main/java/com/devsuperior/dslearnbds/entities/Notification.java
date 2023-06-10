c8f7fa8097f6498361de7522c16eb0b5f4603342
marcosasilva104 <marcosasilva104@gmail.com>
2023-06-10 12:38:34 -0300

    Custom content for current user

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
@@ -24,7 +24,7 @@ public class Notification implements Serializable {
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
-	private Boolean read;
+	private boolean read;
 	private String route;
 
 	@ManyToOne
@@ -34,7 +34,7 @@ public class Notification implements Serializable {
 	public Notification() {
 	}
 	
-	public Notification(Long id, String text, Instant moment, Boolean read, String route, User user) {
+	public Notification(Long id, String text, Instant moment, boolean read, String route, User user) {
 		super();
 		this.id = id;
 		this.text = text;
@@ -68,11 +68,11 @@ public class Notification implements Serializable {
 		this.moment = moment;
 	}
 
-	public Boolean getRead() {
+	public boolean isRead() {
 		return read;
 	}
 
-	public void setRead(Boolean read) {
+	public void setRead(boolean read) {
 		this.read = read;
 	}
 
